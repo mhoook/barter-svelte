843ceecafda5f3df9077f8a765987d145d783bf1
icrazymob <44069831+icrazymob@users.noreply.github.com>
2021-04-17 19:41:30 +0300

    fix mobile media helper

--- src/helpers/media.js
+++ src/helpers/media.js
@@ -1,1 +1,1 @@
-export const checkMobile = () => /Mobi/.test(navigator.userAgent)&&window.innerWidth<768
+export const checkMobile = () => /Mobi/.test(navigator.userAgent)&&(window.innerWidth<768 || window.screen.availWidth < 768)
